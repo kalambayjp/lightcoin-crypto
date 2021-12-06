a0e1360334f63cdd522034b7509eefadc4e62435
JP <kalambayjp@gmail.com>
2021-12-05 18:00:41 -0800

    created class to create an acount, create a deposit and refactored to make a superclass of transactions

--- index.js
+++ index.js
@@ -1,29 +1,58 @@
-let balance = 500.00;
+// let balance = 500.00;
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+// class to create new account
+class Account {
+  constructor(username) {
+    // add a username to identify account
+    this.username = username;
+    // initialize balance at zero
+    this.balance = 0;
   }
+};
 
-  commit() {
-    balance -= this.amount;
+class Transaction {
+  // created superclass to remove the shared code between withdrawal and deposit
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
   }
 
 }
+
+class Deposit extends Transaction {
+// method to execute deposit
+  commit() {
+    this.account.balance += this.amount;
+  }
+};
+
+class Withdrawal extends Transaction {
+ // method to execute withdawal
+  commit() {
+    this.account.balance -= this.amount;
+  }
+};
+
 
 
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+
+
+console.log('Balance:', myAccount.balance);
